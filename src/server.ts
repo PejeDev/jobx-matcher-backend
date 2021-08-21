e78700f5751331d3b9d837c04f1dc71e948288f2
Juan Camilo Castañeda <hi@pejedev.xyz>
2021-08-20 20:05:48 -0500

    fix: bad named import

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import passport from 'passport';
 
-import AuthJwt from './auth/AuthJwt';
+import AuthJwt from './auth/authJwt';
 import { DatabaseConfig as db, AppConfig as app } from './config/index';
 import ClientErrorHandler from './middlewares/ClientErrorHandler';
 import ErrorLogger from './middlewares/ErrorLogger';
